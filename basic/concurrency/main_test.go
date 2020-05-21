74f5445301a6b9d2b1f8281533d007b890df44cd
luotao <luotao@gutou.com>
2020-05-21 18:27:44 +0800

    mock并发测试

---
+++ basic/concurrency/main_test.go
@@ -0,0 +1,79 @@
+package concurrency_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	type result struct {
+		string
+		bool
+	}
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+	for _, url := range urls {
+		go func(url string) {
+			resultChannel <- result{url, wc(url)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+
+	return results
+}
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	actualResults := CheckWebsites(mockWebsiteChecker, websites)
+
+	want := len(websites)
+	got := len(actualResults)
+	if want != got {
+		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+
+	expectedResults := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	if !reflect.DeepEqual(expectedResults, actualResults) {
+		t.Fatalf("Wanted %v, got %v", expectedResults, actualResults)
+	}
+}
+
+func mockSlowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(mockSlowStubWebsiteChecker, urls)
+	}
+}
