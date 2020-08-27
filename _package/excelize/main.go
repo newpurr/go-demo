0d6f89e50a9474646315216961784debee152934
luotao <luotao@gutou.com>
2020-08-27 20:30:28 +0800

    excel包demo

---
+++ _package/excelize/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+// https://xuri.me/excelize/zh-hans/base/installation.html#NewFile
+func main() {
+	// f := excelize.NewFile()
+	// // 创建一个工作表
+	// index := f.NewSheet("Sheet2")
+	// // 设置单元格的值
+	// f.SetCellValue("Sheet2", "A2", "Hello world.")
+	// f.SetCellValue("Sheet1", "B2", 100)
+	// // 设置工作簿的默认工作表
+	// f.SetActiveSheet(index)
+	// // 根据指定路径保存文件
+	// if err := f.SaveAs("Book1.xlsx"); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	f2 := excelize.NewFile()
+	size := 40000
+	wg := sync.WaitGroup{}
+	wg.Add(4)
+	go func() {
+		defer wg.Done()
+		for i := 1; i < size; i++ {
+			fmt.Println("A" + strconv.Itoa(i))
+			f2.SetCellValue("Sheet1", "A"+strconv.Itoa(i), i)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 100; i < 2*size; i++ {
+			fmt.Println("A" + strconv.Itoa(i))
+			f2.SetCellValue("Sheet1", "A"+strconv.Itoa(i), i)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 100; i < 3*size; i++ {
+			fmt.Println("A" + strconv.Itoa(i))
+			f2.SetCellValue("Sheet1", "A"+strconv.Itoa(i), i)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 100; i < 4*size; i++ {
+			fmt.Println("A" + strconv.Itoa(i))
+			f2.SetCellValue("Sheet1", "A"+strconv.Itoa(i), i)
+		}
+	}()
+	wg.Wait()
+	// 根据指定路径保存文件
+	if err := f2.SaveAs("Book2.xlsx"); err != nil {
+		fmt.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module go-demo
 go 1.13
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/davecgh/go-spew v1.1.1
-	github.com/json-iterator/go v1.1.10
-	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.5.1
-	github.com/thinkeridea/go-extend v1.1.1
 	github.com/uniplaces/carbon v0.1.6
 
 )
