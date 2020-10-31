8d987dcf0ad9ab1b1194c71845a7fca16fa93845
luotao <luotao@gutou.com>
2020-10-31 23:59:51 +0800

    sync.Cond

---
+++ _package/sync/main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCond(t *testing.T) {
+	var sharedRsc = false
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	m := sync.Mutex{}
+	c := sync.NewCond(&m)
+	go func() {
+		// this go routine wait for changes to the sharedRsc
+		c.L.Lock()
+		for sharedRsc == false {
+			fmt.Println("goroutine1 wait")
+			c.Wait()
+		}
+		fmt.Println("goroutine1", sharedRsc)
+		c.L.Unlock()
+		wg.Done()
+	}()
+
+	go func() {
+		// this go routine wait for changes to the sharedRsc
+		c.L.Lock()
+		for sharedRsc == false {
+			fmt.Println("goroutine2 wait")
+			c.Wait()
+		}
+		fmt.Println("goroutine2", sharedRsc)
+		c.L.Unlock()
+		wg.Done()
+	}()
+
+	// this one writes changes to sharedRsc
+	time.Sleep(2 * time.Second)
+	c.L.Lock()
+	fmt.Println("main goroutine ready")
+	sharedRsc = true
+	c.Broadcast()
+	fmt.Println("main goroutine broadcast")
+	c.L.Unlock()
+	wg.Wait()
+}
