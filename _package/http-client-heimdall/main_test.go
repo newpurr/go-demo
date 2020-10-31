9227f8a201ef6221265c1a823a04649a891e8ab6
luotao <luotao@gutou.com>
2020-10-31 23:39:23 +0800

    http-client-heimdall

---
+++ _package/http-client-heimdall/main_test.go
@@ -0,0 +1,72 @@
+package http_client_heimdall
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/gojektech/heimdall/v6/httpclient"
+	"github.com/gojektech/heimdall/v6/hystrix"
+)
+
+func TestRequestGet(t *testing.T) {
+	// Create a new HTTP client with a default timeout
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+
+	// Use the clients GET method to create and execute the request
+	res, err := client.Get("http://www.baidu.com", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Heimdall returns the standard *http.Response object
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
+}
+
+func TestRequestGetWithDo(t *testing.T) {
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+
+	// Create an http.Request instance
+	req, _ := http.NewRequest(http.MethodGet, "http://www.baidu.com", nil)
+	// Call the `Do` method, which has a similar interface to the `http.Do` method
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
+}
+
+func TestRequestGetWithHystrix(t *testing.T) {
+	fallbackFn := func(err error) error {
+		fmt.Println("error1:", err)
+		return err
+	}
+
+	// Create a new hystrix-wrapped HTTP client with the command name, along with other required options
+	client := hystrix.NewClient(
+		hystrix.WithHTTPTimeout(10*time.Millisecond),
+		hystrix.WithCommandName("google_get_request"),
+		hystrix.WithHystrixTimeout(1000*time.Millisecond),
+		hystrix.WithMaxConcurrentRequests(30),
+		hystrix.WithErrorPercentThreshold(20),
+		hystrix.WithFallbackFunc(fallbackFn),
+	)
+
+	// Use the clients GET method to create and execute the request
+	res, err := client.Get("http://google.com", nil)
+	if err != nil {
+		fmt.Println("error2:", err)
+		panic(err)
+	}
+
+	// Heimdall returns the standard *http.Response object
+	body, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
+}
--- go.mod
+++ go.mod
@@ -5,13 +5,11 @@ go 1.13
 require (
 	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
 	github.com/davecgh/go-spew v1.1.1
-	github.com/json-iterator/go v1.1.10
+	github.com/gojektech/heimdall/v6 v6.1.0
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.5.1
-	github.com/thinkeridea/go-extend v1.1.1
 	github.com/uniplaces/carbon v0.1.6
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
