13108d497c270980ff4ac0136255151d145cf0f3
luotao <luotao@gutou.com>
2020-12-08 20:09:41 +0800

    etcd 分布式锁

---
+++ _package/etcd/etcd_lock_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3/concurrency"
+)
+
+var (
+	addr1     = "http://127.0.0.1:2379"
+	lockName2 = "my-test-lock"
+)
+
+func TestMutex(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	// etcd地址
+	endpoints := strings.Split(addr1, ",")
+	// 生成一个etcd client
+	cli, err := clientv3.New(clientv3.Config{Endpoints: endpoints})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 生成一个etcd client
+	cli2, err := clientv3.New(clientv3.Config{Endpoints: endpoints})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+	go useLock(cli, 1)  // 测试锁
+	go useLock(cli2, 2) // 测试锁
+
+	// 等待一段时间
+	time.Sleep(time.Duration(rand.Intn(2)) * time.Second)
+}
+
+func useLock(cli *clientv3.Client, num int) {
+	// 为锁生成session
+	s1, err := concurrency.NewSession(cli)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s1.Close()
+	// 得到一个分布式锁
+	locker := concurrency.NewLocker(s1, lockName2)
+
+	// 请求锁
+	log.Println("acquiring lock", num)
+	locker.Lock()
+	log.Println("acquired lock", num)
+
+	// 等待一段时间
+	time.Sleep(time.Duration(rand.Intn(1)) * time.Second)
+	locker.Unlock() // 释放锁
+
+	log.Println("released lock", num)
+}
--- go.mod
+++ go.mod
@@ -3,13 +3,23 @@ module go-demo
 go 1.13
 
 require (
-	github.com/alibaba/sentinel-golang v1.0.0 // indirect
+	github.com/coreos/etcd v3.3.25+incompatible
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/davecgh/go-spew v1.1.1
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/spf13/cast v1.3.1
 	github.com/stretchr/testify v1.5.1
 	github.com/uniplaces/carbon v0.1.6
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	go.uber.org/zap v1.16.0 // indirect
+	google.golang.org/genproto v0.0.0-20201207150747-9ee31aac76e7 // indirectct
+	google.golang.org/grpc v1.27.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
