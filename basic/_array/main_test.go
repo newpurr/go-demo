0f319c6ecfd567c4261c151fd9643378db9bdf3b
atao <luotao954@gmail.com>
2021-02-04 22:48:22 +0800

    45度遍历二维数组

--- basic/_array/main_test.go
+++ basic/_array/main_test.go
@@ -22,3 +22,78 @@ func TestArrayInsert(t *testing.T) {
 		t.Error("插入错误")
 	}
 }
+
+// 将array[0][0]右下方45度线的值全部替换成2
+func Test1(t *testing.T) {
+	array := [5][4]int{
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+	}
+
+	i := 0 + 1
+	j := 0 + 1
+	for i < len(array) && j < 4 {
+		array[i][j] = 2
+
+		i++
+		j++
+	}
+
+	for _, ints := range array {
+		fmt.Println(ints)
+	}
+
+}
+
+// 将array[4[3]左上方45度线的值全部替换成2
+func Test2(t *testing.T) {
+	array := [5][4]int{
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+	}
+
+	i := 4 - 1
+	j := 3 - 1
+	for i >= 0 && j >= 0 {
+		array[i][j] = 2
+
+		i--
+		j--
+	}
+
+	for _, ints := range array {
+		fmt.Println(ints)
+	}
+
+}
+
+// 将array[4[3]右上方45度线的值全部替换成2
+func Test3(t *testing.T) {
+	array := [5][4]int{
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+		{1, 1, 1, 1},
+	}
+
+	i := 4 - 1
+	j := 0 + 1
+	for i >= 0 && j < 4 {
+		array[i][j] = 2
+
+		i--
+		j++
+	}
+
+	for _, ints := range array {
+		fmt.Println(ints)
+	}
+
+}
