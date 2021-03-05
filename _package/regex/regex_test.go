dc5e1ed000c5caeaac5221d958355002dca3cf6a
atao <luotao954@gmail.com>
2021-03-05 17:21:54 +0800

    更新依赖, 添加正则表达式demo

---
+++ _package/regex/regex_test.go
@@ -0,0 +1,45 @@
+package regex
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+// Go正则表达式示例: https://colobu.com/2020/11/11/golang-regex-replace-example/
+
+// https://gowalker.org/regexp#MatchString
+func TestRegexMatchString(t *testing.T) {
+	str := "Golang regular expressions example"
+	match, err := regexp.MatchString(`^Golang`, str)
+	fmt.Println("Match: ", match, " Error: ", err)
+}
+
+func TestCompile(t *testing.T) {
+	str := "Golang expressions example"
+
+	// Compile() 或者 MustCompile()创建一个编译好的正则表达式对象。假如正则表达式非法，那么Compile()方法回返回error,而MustCompile()编译非法正则表达式时不会返回error，而是回panic。
+	// 如果你想要很好的性能，不要在使用的时候才调用Compile()临时进行编译，而是预先调用Compile()编译好正则表达式对象
+	compile, err := regexp.Compile("Gola([a-z]+)g")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(compile.FindString(str))
+}
+
+// FindAllString
+// FindString方法的All版本，它返回所有匹配的字符串的slice。如果返回nil值代表没有匹配的字符串
+func TestFindAllString(t *testing.T) {
+	str := "Golang regular expressions example"
+
+	compile, err := regexp.Compile(`p([a-z]+)e`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	match := compile.FindAllString(str, 2)
+
+	fmt.Println("Match: ", match, " Error: ", err)
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/davecgh/go-spew v1.1.1
+	github.com/garyburd/redigo v1.6.2 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/gojektech/heimdall/v6 v6.1.0 // indirect
@@ -22,7 +23,7 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/sirupsen/logrus v1.7.0 // indirect
 	github.com/spf13/cast v1.3.1
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
 	github.com/thinkeridea/go-extend v1.2.2 // indirect
 	github.com/uniplaces/carbon v0.1.6
 	go.uber.org/zap v1.16.0 // indirect
