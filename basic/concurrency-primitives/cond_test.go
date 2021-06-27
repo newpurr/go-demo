a74232be4584b9df6e508724adb9fd2fca9bda76
atao <luotao954@gmail.com>
2021-06-27 22:35:01 +0800

    sync.cond 使用测试用例

--- basic/concurrency-primitives/cond_test.go
+++ basic/concurrency-primitives/cond_test.go
@@ -37,3 +37,41 @@ func TestCondSimple(t *testing.T) {
 	// 所有的运动员是否就绪
 	log.Println("所有运动员都准备就绪。比赛开始，3，2，1, ......")
 }
+
+func TestCondBroadcast(t *testing.T) {
+	var (
+		m      sync.Mutex
+		c      = sync.NewCond(&m)
+		wg     = sync.WaitGroup{} // 确定所有工作协程准备就绪
+		wgDone = sync.WaitGroup{} // 确定所有工作协程工作完成退出
+		n      = 200              // 工作协程数
+		await  = true             // 统一信号条件
+	)
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		wgDone.Add(1)
+		go func(g int) {
+			defer wgDone.Done()
+			log.Printf("运动员#%d 已准备就绪\n", g)
+			m.Lock()
+			wg.Done()
+			for await {
+				c.Wait()
+			}
+			m.Unlock()
+			log.Printf("运动员#%d 飞一般的冲了出去...\n", g)
+		}(i)
+	}
+
+	wg.Wait()
+	log.Println("裁判员确定运动员准备完毕")
+
+	m.Lock()
+	await = false
+	log.Println("裁判员打响了信号枪")
+	c.Broadcast()
+	m.Unlock()
+
+	//time.Sleep(3 * time.Second)
+	wgDone.Wait()
+}
